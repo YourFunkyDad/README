33469a9abaac43b8bd9831eeb47187ea772c45c0
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-03-24 16:14:06 -0400

    More Questions

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: (value)=>{if (value){return true} else {return 'Error: Must not be left blank.'}}
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: (value)=>{if (value){return true} else {return 'Error: Must not be left blank.'}}
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: (value)=>{if (value){return true} else {return 'Error: Must not be left blank.'}}
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address.',
+        validate: (value)=>{if (value){return true} else {return 'Error: Must not be left blank.'}}
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username.',
+        validate: (value)=>{if (value){return true} else {return 'Error: Must not be left blank.'}}
+    },
+    
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPL', 'Apache', 'GNU General Public', 'Mozilla-Public', 'None']
+    }
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who all has contributed to this project?',
+        validate: (value)=>{if (value){return true} else {return 'Error: Must not be left blank.'}}
+    },
+    
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
